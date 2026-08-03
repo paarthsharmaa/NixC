8b9a69f7c909d0bb81ec068fddf92d51ebd68fb9
Paarth Sharma <paarthsharma0912@gmail.com>
2026-08-04 00:09:30 +0530

    kitty error fixed

--- modules/components/terminal/kitty/default.nix
+++ modules/components/terminal/kitty/default.nix
@@ -3,42 +3,57 @@
   moduleWithSystem,
   ...
 }: {
-		flake.nixosModules.kitty = moduleWithSystem ({self', ...}:
-			{...}: {
-      environment.systemPackages = with self'.packages; [
-      kitty
-    ];
-  });
+  flake.nixosModules.kitty = moduleWithSystem (
+    {self', ...}: {...}: {
+      environment.systemPackages = [
+        self'.packages.kitty
+      ];
+    }
+  );
+
   perSystem = {pkgs, ...}: {
     packages.kitty = let
-      jetbrains-mono = pkgs.nerd-fonts.jetbrains-mono;
+      jetbrainsMono =
+        pkgs.nerd-fonts.jetbrains-mono;
+
       fontsConf = pkgs.makeFontsConf {
-        fontDirectories = [jetbrains-mono];
+        fontDirectories = [
+          jetbrainsMono
+        ];
       };
+    in
       inputs.wrappers.wrappers.kitty.wrap {
-				in
         inherit pkgs;
+
         environment = {
-          "FONTCONFIG_FILE" = "${fontsConf}";
+          FONTCONFIG_FILE = "${fontsConf}";
         };
+
         font = {
-          name = "JetbrainsMono Nerd Font Mono";
+          name = "JetBrainsMono Nerd Font Mono";
           size = 12;
         };
+
         settings = {
+          scrollback_lines = 10000;
+
           window_padding_width = 9;
           background_opacity = 0.50;
           confirm_os_window_close = 0;
           enable_audio_bell = false;
+
           cursor_trail = 1;
           cursor_trail_start_threshold = 1;
           cursor_trail_color = "#cba6f7";
           cursor_shape = "beam";
+
           allow_remote_control = true;
         };
+
         keybindings = {
           "ctrl+backspace" = "send_text all \\x17";
         };
+
         themeFile = "Catppuccin-Mocha";
       };
   };
