d09315a9e238bacb1a2d1d0f41d913d0d068e2ec
Paarth Sharma <paarthsharma0912@gmail.com>
2026-09-02 01:16:37 +0530

    disabled yoga conservation mode, tmux config fixes

--- modules/machines/yoga/configuration.nix
+++ modules/machines/yoga/configuration.nix
@@ -2,5 +2,16 @@
   flake.nixosModules.yogaConfiguration = {...}: {
     networking.hostName = "yoga";
     system.stateVersion = "26.05";
+
+    # Disable Lenovo battery conservation mode (60% limit)
+    systemd.services.disable-conservation-mode = {
+      description = "Disable Lenovo battery conservation mode";
+      serviceConfig = {
+        Type = "oneshot";
+        ExecStart = "/bin/sh -c 'echo 0 > /sys/bus/platform/drivers/ideapad_acpi/VPC2004:00/conservation_mode'";
+      };
+      wantedBy = ["multi-user.target"];
+      after = ["sys-subsystem-platform-drivers-ideapad_acpi-VPC2004:00.device"];
+    };
   };
 }
